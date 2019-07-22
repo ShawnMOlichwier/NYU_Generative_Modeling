4774d9691db4c0f779e15dae2a8bba8f20dcf551
Shawn Olichwier <shawnolich@gmail.com>
2019-07-22 19:49:49 -0400

    Update RNN_Decoder.py

--- RNN_Decoder.py
+++ RNN_Decoder.py
@@ -20,3 +20,25 @@ class RNN(torch.nn.Module):
     def forward(self, x):
         x = self.fc1(x)
         return x
+
+
+
+'''
+def LSTMCELL(prev_ct, prev_ht, input):
+    combine = prev_ht + input
+    ft = forget_layer(combine)
+    candidate = candidate_layer(combine)
+    it = input_layer(combine)
+    Ct = prev_ct * ft + candidate * it
+    ot = output_layer(combine)
+    ht = ot * tanh(Ct)
+    return ht, Ct
+
+
+ct = [0,0,0]
+ht = [0,0,0]
+
+for input in inputs:
+    ct, ht = LSTMCELL(ct, ht, input)
+
+'''
